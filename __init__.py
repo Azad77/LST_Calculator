3ba800f864e2e0b2cc767891e1a1ee17f1ad9b3f
Dr Azad Rasul <azad977@gmail.com>
2025-02-17 16:28:02 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+"""
+Author: Dr. Azad Rasul
+Affiliation: Soran University
+Email: azad.rasul@soran.edu.iq
+Year: 2025
+"""
 from .lst_plugin import LSTCalculator
 def classFactory(iface):
     return LSTCalculator(iface)
